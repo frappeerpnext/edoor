999eabb52dcc37c718ea923485a8feb6eb19aec9
frappeerpnext <frapperpnext@gmail.com>
2023-07-27 14:13:27 +0700

    update edoor

--- frontdesk/src/views/TestPage2.vue
+++ frontdesk/src/views/TestPage2.vue
@@ -1,6 +1,6 @@
 <template>
-    <div>
-        <TestPage :list="msg" :test="test">
+    <div ref="input">
+        <TestPage :list="msg" :test="test" >
             <template v-slot="slotProps">
                 <div class="text-red-400">{{ slotProps.data }}</div>
             </template>
@@ -13,6 +13,8 @@ import {ref} from 'vue'
 import TestPage from './TestPage.vue'
 var msg = ref(['A','B','C'])
 const test = ref('v')
+const input = ref()
+console.log(input)
 function onClick(){
     test.value = 'ww'
 }
