591b6a2c8f132863bbd9252678d047ba4173187e
Pheakdey <frapperpnext@gmail.com>
2023-08-30 15:03:18 +0700

    ratha update 2023-08-30 3:02 PM

--- frontdesk/src/utils/socketio.js
+++ frontdesk/src/utils/socketio.js
@@ -1,18 +1,13 @@
-
-
 import io from 'socket.io-client';
 import { websocket_port } from "../../../../../sites/common_site_config.json"
 
 let host = window.location.hostname;
 let port = ":" +  websocket_port;//window.location.port ? ':9004' : '';
  
-let protocol = window.location.protocol;
- 
-let url = `${protocol}//${host}${port}`;
+let protocol = port ? 'http' : 'https';
+let url = `${protocol}://${host}${port}`;
  
 let socket = io(url);
 
 
 export default socket;
-
-
