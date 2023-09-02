34536e42475c694bdac8f3e7a0473c3c05b9b77b
Pheakdey <frapperpnext@gmail.com>
2023-09-02 15:54:46 +0700

    update socket

--- frontdesk/testsocket.cjs
+++ frontdesk/testsocket.cjs
@@ -5,19 +5,18 @@ const fs = require('fs');
 const wss = new WebSocket.Server({
   port: 3001,
   secure: true,
-  // The path to the SSL certificate file.
-  cert: fs.readFileSync('/etc/letsencrypt/live/www.ebad.ewebcloudserver.com/fullchain.pem'),
+  //The path to the SSL certificate file.
+  cert: fs.readFileSync('fullchain.pem'),
   // The path to the SSL private key file.
-  key: fs.readFileSync('/etc/letsencrypt/live/www.ebad.ewebcloudserver.com/privkey.pem')
+  key: fs.readFileSync('privkey.pem')
 });
 
 wss.on('connection', function connection(ws) {
+  
   ws.on('message', function incoming(message) {
     console.log('received: %s', message);
-
     ws.send('I got message from client and send it back');
-    
   });
- 
   ws.send('Hello from server!');
+
 })
