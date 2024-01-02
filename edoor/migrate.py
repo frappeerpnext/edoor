a1175e458fbd17df211feb1f84696f20cbdb4f84
outdomleap <outdom.leap@esoftix.com>
2024-01-02 19:03:32 +0700

    update edoor

--- edoor/migrate.py
+++ edoor/migrate.py
@@ -1,3 +1,11 @@
 import frappe
 def after_migrate():
-    frappe.msgprint("Update Complete")
+    comment_doc = [{
+        "subject": "Change Value",
+        "custom_audit_trail_type":"Updated",
+        "custom_icon":"pi pi-file-edit",
+        "reference_doctype":"Customer",
+        "reference_name":"0001",
+        "content":"this is from after update"
+        }]
+    frappe.enqueue("edoor.api.utils.add_audit_trail", queue='short', data=comment_doc)
