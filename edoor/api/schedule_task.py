c8abe515ac0ff3c5a0b5ddc6517f83cec3d24964
outdomleap <outdom.leap@esoftix.com>
2024-01-20 12:41:06 +0700

    update edoor

--- edoor/api/schedule_task.py
+++ edoor/api/schedule_task.py
@@ -45,7 +45,7 @@ def re_run_fail_jobs():
     jobs =  sorted(jobs, key=lambda j: j.modified, reverse=order_desc)
 
     # jobs = [d for d in jobs if d["job_name"] =='edoor.edoor.doctype.reservation_stay.reservation_stay.generate_room_occupy']
-    jobs = [d for d in jobs  if  ['Deadlock found when trying', 'Lock wait timeout exceeded'] in  d["exc_info"] ]
+    jobs = [d for d in jobs  if  ['Deadlock found when trying', 'Lock wait timeout exceeded','Document has been modified after you have opened it'] in  d["exc_info"] ]
     job_ids = []
     for j in jobs:
         job =   json.loads(j["arguments"]) 
