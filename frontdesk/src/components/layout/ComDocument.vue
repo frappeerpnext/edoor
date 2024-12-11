ca5febd7f4160cdeaa5ef19dcb7e89f8d83527bc
frappeerpnext <outdom.leap@esoftix.com>
2024-12-11 08:45:08 +0700

    update edoor

--- frontdesk/src/components/layout/ComDocument.vue
+++ frontdesk/src/components/layout/ComDocument.vue
@@ -81,6 +81,7 @@
             <OverlayPanel ref="opEdit">
                 <ComOverlayPanelContent :loading="saving" @onCancel="onEdit($event, {})" @onSave="onSave">
                     <div class="mb-2">
+                        HIII
                         <label>{{ $t('Title') }} </label><br />
                         <InputText type="text" class="p-inputtext-sm w-full" :placeholder="$t('Title')"
                             v-model="selected.custom_title" />
